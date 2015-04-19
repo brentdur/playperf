f7f050d54d83c3397f28e21981ea580104070c67
Brenton Durkee <brentdur@gmail.com>
2015-04-18 22:41:05 -0400

    changing music frameworks

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,32 +1,84 @@
 'use strict';
 
 angular.module('musicApp')
-  .controller('MainCtrl', function ($scope, $http, songza, ngAudio) {
-    $scope.data = songza.stations;
+  .controller('MainCtrl', function ($scope, $http, songza, soundcloud, ngAudio, $timeout) {
+    $scope.stations = songza.stations;
+    $scope.tracks = soundcloud.tracks;
+    $scope.playable = [];
     $scope.link = songza.link;
-    $scope.loaded = false;
+    $scope.loaded = true;
     $scope.loading = false;
     $scope.paused = false;
     $scope.controlText = 'Pause';
     $scope.audio = null;
+    $scope.current;
+
     $scope.send = function(){
-      songza.searchStations($scope.term);
+      songza.searchStations($scope.term).success(function(){
+        for(var i = 0; i < $scope.stations.length; i++){
+          $scope.playable.push({type: 'songza', item: $scope.stations[i]});
+        }
+      });
+      soundcloud.searchTracks($scope.soundTerm).success(function(){
+        for(var i = 0; i < $scope.tracks.length; i++){
+          $scope.playable.push({type: 'cloud', item: $scope.tracks[i]});
+        }
+        $scope.getListen();
+      });
+      $scope.term = '';
     };
 
     $scope.getListen = function(){
       $scope.loading = true;
       if($scope.audio != null){
           $scope.audio.stop();
-        }
-      songza.getListen($scope.stationId).success(function(data){
-        $scope.link = songza.song.listen_url;
-        $scope.songTitle = songza.song.song.title;
-        $scope.audio = ngAudio.play($scope.link);
-        $scope.loaded = true;
-        console.log($scope.audio.paused)
-        $scope.loading = false;
+      }
+      var choose =  Math.floor(Math.random() * $scope.playable.length);
+      var item = $scope.playable[choose];
+      $scope.playable.splice(choose, 1);
+      $scope.current = item;
+      if(item.type === 'songza'){
+        $scope.songzaPlay(item.item);
+      }
+      else if(item.type === 'cloud'){
+        $scope.soundCloudPlay(item.item);
+      }
+    };
+
+    $scope.finishLoading = function(){
+      // $scope.loaded = true;
+      $scope.audio = ngAudio.load($scope.link);
+      // $scope.loading = false;
+      $timeout(function(){
+        $scope.audio.play();
+      }, '5000');  
+    };
+
+    $scope.songzaPlay = function(obj){
+      songza.getListen(obj.id).success(function(data){
+        $scope.link = obj.listen_url;
+        $scope.songTitle = obj.title;
+        $scope.finishLoading();
       });
     };
+
+    $scope.soundCloudPlay = function(obj){
+      $scope.link = obj.stream_url+'?client_id='+$scope.clientID();
+      $scope.songTitle = obj.title;
+      console.log($scope.link, $scope.songTitle);
+      $scope.finishLoading();
+
+    };
+
+    $scope.clientID = function(){
+      return 'c8cff8892431fa994f15e719dc19e6ef';
+    };
+
+    $scope.$watch('audio.remaining', function(){
+      if($scope.audio.remaining <= 0){
+        $scope.getListen();
+      }
+    });
 
     $scope.toggleAudio = function(){
       if(!$scope.paused){
--- server/api/soundcloud/index.js
+++ server/api/soundcloud/index.js
@@ -15,24 +15,23 @@ var request = require('request');
 //router.patch('/:id', controller.update);
 //router.delete('/:id', controller.destroy);
 
-router.post('/', function(req, res) {
-	request('http://www.google.com', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			return res.json(200, body);
-	  }
-	});
-});
+// router.post('/', function(req, res) {
+// 	request('http://www.google.com', function (error, response, body) {
+// 		if (!error && response.statusCode == 200) {
+// 			return res.json(200, body);
+// 	  }
+// 	});
+// });
 
-/*
-router.post('/playlist', function(req, res) {
-	request('http://api.soundcloud.com/playlists.json?client_id=c8cff8892431fa994f15e719dc19e6ef&tags=' + req.body.genre.toString(), function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			return res.json(200, JSON.parse(body));
-		}
 
-	});
-});
-*/
+// router.post('/playlist', function(req, res) {
+// 	request('http://api.soundcloud.com/playlists.json?client_id=c8cff8892431fa994f15e719dc19e6ef&tags=' + req.body.genre.toString(), function (error, response, body) {
+// 		if (!error && response.statusCode == 200) {
+// 			return res.json(200, JSON.parse(body));
+// 		}
+
+// 	});
+// });
 
 router.post('/tracks', function(req, res) {
 	request('http://api.soundcloud.com/tracks.json?client_id=c8cff8892431fa994f15e719dc19e6ef&tags=' + req.body.genre, function (error, response, body) {
